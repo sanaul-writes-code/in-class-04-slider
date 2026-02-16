63e1d8150edf87290c326a932272714500521c59
sanaul-writes-code <165845997+sanaul-writes-code@users.noreply.github.com>
2026-02-16 18:12:24 -0500

    level 1 complete with increment and reset buttons implemented

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stateful Lab',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0; // This is our STATE
+
+  void increment() {
+    setState(() {
+      _counter += 1;
+    });
+  }
+
+  void reset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Interactive Counter')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue.shade100,
+              padding: EdgeInsets.all(20),
+              child: Text(
+                '$_counter',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Slider(
+            min: 0, max: 100,
+            value: _counter.toDouble(),
+            onChanged: (double value) {
+              // 👇 This triggers the UI rebuild
+              setState(() {
+                _counter = value.toInt();
+              });
+            },
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            ElevatedButton(onPressed: increment, child: Text('+')),
+            SizedBox(width: 8,),
+            ElevatedButton(onPressed: reset, child: Text('Reset')),
+          ],)
+        ],
+      ),
+    );
+  }
+}
